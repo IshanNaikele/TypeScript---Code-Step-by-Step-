808fc1cc2c19266295677923b2ddf0da9bf61703
IshanNaikele <ishannaikele23@gmail.com>
2025-08-31 14:34:38 +0530

    40 min completed

---
+++ first1.js
@@ -0,0 +1,6 @@
+var age = 20; // It is not mandatory to include the datatype everytime ,it's depends on yourself
+console.log(age);
+// age="Ishan Naikele"   --->These line will create error here .
+console.log(age);
+var name = "Ishan";
+console.log(name);
---
+++ first1.ts
@@ -0,0 +1,9 @@
+var age:number =20; // It is not mandatory to include the datatype everytime ,it's depends on yourself
+console.log(age);
+
+// age="Ishan Naikele"   --->These line will create error here .
+console.log(age)
+
+
+var name="Ishan"
+console.log(name)
